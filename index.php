506d6594b5d4471f067cdb8de7d04dedbc7aa7bd
Kiki Moorty <keertana.moorty@gmail.com>
2017-03-04 20:58:26 +0000

    i love cats

---
+++ index.php
@@ -0,0 +1,13 @@
+<!-- This is the basic webpage-->
+<!-- We call it a hypertext markup language document -->
+<!DOCTYPE html>
+<!-- set default language -->
+<html lang="en">
+<!-- header: stuff we reference will go here (Libraries, Stylesheet stuff) -->
+<head>  
+</head> 
+<!-- body: where we will put the stuff we can see! -->
+<body>
+<h1>Hello World!</h1>     
+</body>
+
