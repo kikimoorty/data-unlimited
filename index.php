95b578249c2aecc55c83d2fd34baeeedb74d8e1f
Kiki Moorty <keertana.moorty@gmail.com>
2017-03-04 21:15:57 +0000

    I really love cats

--- index.php
+++ index.php
@@ -4,10 +4,18 @@
 <!-- set default language -->
 <html lang="en">
 <!-- header: stuff we reference will go here (Libraries, Stylesheet stuff) -->
-<head>  
+<head> 
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1"> 
+
+  <!-- These scripts give us access to the bootstrap font end framework, which makes styling pretty easy!  -->
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 </head> 
 <!-- body: where we will put the stuff we can see! -->
 <body>
-<h1>Hello World!</h1>     
+  <!-- suddenly, we can style our stuff with bootstrap classes!  -->
+    <h1>Hello World!</h1> 
+      
 </body>
-
