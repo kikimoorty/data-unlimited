f7efae432db37381bd1e588dd67f6fab88962335
Kiki Moorty <keertana.moorty@gmail.com>
2017-03-04 21:34:39 +0000

    EXO is cool too

--- index.php
+++ index.php
@@ -1,23 +1,25 @@
-<!-- This is the basic webpage-->
+<!-- This is the basic webpage--> 
 <!-- We call it a hypertext markup language document -->
 <!DOCTYPE html>
-<!-- set default language -->
 <html lang="en">
-<!-- header: stuff we reference will go here (Libraries, Stylesheet stuff) -->
 <head> 
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1"> 
-
-  <!-- These scripts give us access to the bootstrap font end framework, which makes styling pretty easy!  -->
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-</head> 
-<!-- body: where we will put the stuff we can see! -->
+
+</head>
 <body>
-  <!-- let's add more! We'll put in some sections that we can fill with our examples!  -->
-    <h1>My Page</h1>
-    <div id = "section">
+  <!-- Let's add header and footer!  -->
+  <div id= "navbar"> 
+     <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <h1> My Page </h1>
+        </div>
+      </nav>
+   </div>   
+  <div id = "section">
   
     <div class= "container">
       <div class = "jumbotron">
@@ -54,7 +56,15 @@
     </div> 
 
   </div>
-<!-- End Charts -->
+<!-- Let's add header & Footer -->
+  <footer>
+      <div class="container">
+        <div id="section">
+          <div class="row">
+            <p class="copyright"> &copy 2017 Friendly Fulcrum </p>
+          </div>
+        </div>  
+      </div>
+  </footer>
       
 </body>
-Contact GitHub API Training Shop Blog About
